c20ca8572671c802cc1f280e1797a89c9bf92585
xuanzi820 <youngclyang@163.com>
2019-06-23 22:03:50 +0800

    用户登录-发送验证码-手机号码校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -100,6 +100,17 @@ export default {
       })
     },
     handleSendCode() {
+      // 检验手机号是否有效
+      this.$refs['ruleForm'].validateField('mobile', errorMessage => {
+        // console.log('errorMessage => ', errorMessage)
+        if (errorMessage.trim().length > 0) {
+          return
+        }
+        // 手机号码有效，初始化验证码插件
+        this.showGeetest()
+      })
+    },
+    showGeetest() {
       const { mobile } = this.form
       if (this.capchaObj) {
         return this.capchaObj.verify()
