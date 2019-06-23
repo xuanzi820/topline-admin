69e7248b325278fa87016bee5ef5e7af4ab1b3df
xuanzi820 <youngclyang@163.com>
2019-06-23 11:36:29 +0800

    用户登录-禁用登录过程中的登录按钮

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,11 +5,11 @@
                 <img src="./logo_index.png" alt="">
             </div>
             <div class="login-form">
-                <el-form ref="form" :model="form">
-                    <el-form-item>
+                <el-form :model="form"  :rules="rules" ref="ruleForm" >
+                    <el-form-item prop="mobile">
                         <el-input v-model="form.mobile" placeholder="请输入手机号"></el-input>
                     </el-form-item>
-                    <el-form-item>
+                    <el-form-item prop="code">
                         <!-- 支持栅格布局，一共是24列 -->
                         <el-col :span="12">
                             <el-input v-model="form.code" placeholder="验证码"></el-input>
@@ -20,7 +20,12 @@
                     </el-form-item>
                     <el-form-item>
                         <!-- 给组件加class,会作用到它的根元素 -->
-                        <el-button class="btn-login" type="primary" @click="handleLogin">登录</el-button>
+                        <el-button
+                        class="btn-login"
+                        type="primary"
+                        @click="handleLogin"
+                        :loading="loginLoading"
+                        >登录</el-button>
                     </el-form-item>
                 </el-form>
             </div>
@@ -35,20 +40,41 @@ export default {
   name: 'AppLogin',
   data() {
     return {
-      form: {
+      form: { // 表单数据
         mobile: '15733298674',
         code: ''
+      },
+      loginLoading: false, // 登录按钮的loading状态
+      rules: { // 表单验证规则
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { min: 11, max: 11, message: '长度必须为11位', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { min: 6, max: 6, message: '长度必须为6位', trigger: 'blur' }
+        ]
       },
       capchaObj: null // 通过 initGeetest 得到的极验验证码对象
     }
   },
   methods: {
     handleLogin() {
+      this.$refs['ruleForm'].validate((valid) => {
+        if (!valid) {
+          return
+        }
+        // 表单验证通过，提交登录
+        this.submitLogin()
+      })
+    },
+    submitLogin() {
+      this.loginLoading = true
       axios({
         method: 'POST',
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
         data: this.form
-      }).then(res => { // 状态码>=200&&<400的都会进入此处
+      }).then(res => { // 状态码>=200 && <400的都会进入此处
         this.$message({
           message: '登陆成功',
           type: 'success'
@@ -56,10 +82,12 @@ export default {
         this.$router.push({
           name: 'home'
         })
+        this.loginLoading = false
       }).catch(err => { // 状态码>=400的都会进入此处
         if (err.response.status === 400) {
           this.$message.error('登陆失败，手机号或验证码错误')
         }
+        this.loginLoading = false
       })
     },
     handleSendCode() {
