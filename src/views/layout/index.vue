03edb83fe5aad4a7cb686c4b9dc5d9208f319708
xuanzi820 <youngclyang@163.com>
2019-06-24 11:43:23 +0800

    Layout组件-Container布局容器

---
+++ src/views/layout/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <el-container>
+      <el-aside width="200px"><AppAside/></el-aside>
+      <el-container>
+        <el-header><AppHeader/></el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+    <!-- 这里是内容是 Home、发布文章、评论管理。。。。 -->
+</template>
+<script>
+import AppAside from './components/AppAside'
+import AppHeader from './components/AppHeader'
+export default {
+  name: 'AppLayout',
+  components: {
+    AppAside,
+    AppHeader
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
+.el-aside {
+  background-color: #D3DCE6;
+}
+.el-header {
+  background-color: #B3C0D1;
+}
+.el-main{
+  background-color: #e9eef3;
+}
+</style>
