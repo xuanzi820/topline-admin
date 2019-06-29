4e96a8ac648f69c51dce01c7515ad555e9f48b7f
xuanzi820 <youngclyang@163.com>
2019-06-29 22:32:21 +0800

    文章筛选-基本完成

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -7,16 +7,18 @@
       </div>
       <el-form ref="form" :model="form" label-width="80px">
         <el-form-item label="状态">
-          <el-radio-group v-model="form.resource">
+          <el-radio-group v-model="filterParams.status">
+            <el-radio label="">全部</el-radio>
             <el-radio
-              v-for="item in statTypes"
+              v-for="(item,index) in statTypes"
               :key="item.label"
-              :label="item.label">
+              :label="index+''"
+              >{{item.label}}
             </el-radio>
           </el-radio-group>
         </el-form-item>
         <el-form-item label="频道">
-          <el-select v-model="form.region" placeholder="请选择活动区域">
+          <el-select v-model="filterParams.channel_id" placeholder="请选择活动区域">
             <el-option
               v-for="item in channels"
               :key="item.id"
@@ -27,7 +29,9 @@
         </el-form-item>
         <el-form-item label="时间">
           <el-date-picker
-            v-model="form.value1"
+            value-format="yyyy-MM-dd"
+            v-model="begin_end_pubdate"
+            @change="handleDateChange"
             type="daterange"
             range-separator="至"
             start-placeholder="开始日期"
@@ -35,7 +39,11 @@
           </el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">查询</el-button>
+          <el-button
+            type="primary"
+            @click="onSubmit"
+            :loading="articleLoading"
+          >查询</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -43,7 +51,7 @@
     <!-- 列表 -->
     <el-card class="list-card">
       <div slot="header" class="clearfix">
-        <span>共找到15条符合条件的内容</span>
+        <span>共找到<strong>{{totalCount}}</strong>条符合条件的内容</span>
       </div>
       <!-- table表格 -->
       <!-- data用来指定表格的数据
@@ -54,7 +62,7 @@
       <el-table
         class="list-table"
         :data="articles"
-        v-loading="articleLoading"
+        :loading="articleLoading"
         style="width: 100%">
         <el-table-column
           prop="cover.images[0]"
@@ -123,21 +131,10 @@ export default {
   data() {
     return {
       articles: [], // 列表数据
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: '',
-        value1: ''
-      },
-      totalCount: 0,
-      articleLoading: false,
-      page: 1,
-      statTypes: [
+      totalCount: 0, // 总数据条数
+      articleLoading: false, // 控制文章加载中的loading效果
+      page: 1, // 当前页码
+      statTypes: [ // 文章状态
         {
           type: 'info',
           label: '草稿'
@@ -159,7 +156,14 @@ export default {
           label: '已删除'
         }
       ],
-      channels: [] // 频道列表
+      channels: [], // 频道列表
+      filterParams: { // 文章查询条件参数
+        status: '', // 文章状态
+        channel_id: '', // 频道id
+        begin_pubdate: '', // 起始时间
+        end_pubdate: '' // 截止时间
+      },
+      begin_end_pubdate: [] // 存储日期选择器同步的[开始时间，结束时间]，这个字段没啥用，只是时间选择器必须v-model绑定一个数据才能触发change事件
     }
   },
   created() {
@@ -171,12 +175,24 @@ export default {
   methods: {
     loadArticles(page = 1) {
       this.articleLoading = true
+      // 过滤出有效的查询条件数据字段
+      const filterData = {}
+      for (let key in this.filterParams) {
+        if (this.filterParams[key]) {
+          filterData[key] = this.filterParams[key]
+        }
+      }
+
+      console.log(filterData)
+
       this.$http({
         method: 'GET',
         url: '/articles',
         params: {
           page, // 请求数据的页码，不传默认为1
-          per_page: 10// 请求数据的每页大小，不传默认为10
+          per_page: 10, // 请求数据的每页大小，不传默认为10
+          // 将对象混入当前对象,说白就是对象拷贝
+          ...filterData
         }
       }).then(data => {
         this.articleLoading = false
@@ -194,7 +210,8 @@ export default {
       })
     },
     onSubmit() {
-      console.log('submit!')
+      // this.page = 1
+      this.loadArticles()
     },
     handleCurrentChange(page) {
       this.page = page
@@ -228,6 +245,11 @@ export default {
           message: '已取消删除'
         })
       })
+    },
+    handleDateChange(value) {
+      this.filterParams.begin_pubdate = value[0]
+      this.filterParams.end_pubdate = value[1]
+      // console.log(value)
     }
   }
 }
