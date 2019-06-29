a2c6faddbecb86d140f1f5e85b55e6dfadfb4154
xuanzi820 <youngclyang@163.com>
2019-06-30 01:17:54 +0800

    发布文章-布局

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,17 +1,54 @@
 <template>
-  <div>
-    <p>Publish Component</p>
-  </div>
+  <el-card class="publish-card">
+    <div slot="header" class="header">
+      <span>发布文章</span>
+      <div>
+        <el-button type="success">发布</el-button>
+        <el-button type="primary">存入草稿</el-button>
+      </div>
+    </div>
+    <el-form>
+      <el-form-item label="标题">
+        <el-input type="text" v-model="articleForm.title"></el-input>
+      </el-form-item>
+      <el-form-item label="内容">
+        <el-input type="textarea" v-model="articleForm.content"></el-input>
+      </el-form-item>
+      <el-form-item label="封面">
+      </el-form-item>
+      <el-form-item label="频道">
+        <el-select v-model="articleForm.channel_id">
+          <el-option label="区域一" value="shanghai"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 <script>
 export default {
   name: 'AppPublish',
   data() {
     return {
-
+      articleForm: {
+        title: '', // 标题
+        content: '', // 内容
+        cover: { // 封面
+          type: 0, // 封面类型 -1：自动，0-无图，1-1张，3-3张
+          images: [] // 图片链接
+        },
+        channel_id: 3 // 频道
+      }
     }
   }
 }
 </script>
 <style lang="less" scoped>
+.publish-card {
+  min-height: 100%;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
 </style>
