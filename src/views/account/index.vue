c0829eabedc3170414693d10955b9a3b6fa7c6d6
xuanzi820 <youngclyang@163.com>
2019-07-02 12:00:08 +0800

    账户设置-用户头像-配置使用upload上传组件

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -29,7 +29,26 @@
           </el-form-item>
         </el-form>
       </el-col>
-      <el-col :offset="2" :span="4"></el-col>
+      <el-col :offset="2" :span="4">
+        <!-- el-upload上传组件，它会自动将用户选择的图片去请求上传，我们要做的就是配置一下
+        action请求地址
+        由于它用的是自己内部的请求，不是使用的axios去发请求
+          完成路径
+          它的请求也不会经过axios拦截器，所以需要手动设置token
+        可惜的是它不支持自定义请求方法，前功尽弃
+         -->
+        <el-upload
+          class="avatar-uploader"
+          action="http://ttapi.research.itcast.cn/mp/v1_0/user/profile"
+          :headers="{Authorization: token}"
+          name='photo'
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload">
+          <img v-if="userInfo.photo" :src="userInfo.photo" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-col>
     </el-row>
   </el-card>
 </template>
@@ -39,7 +58,8 @@ export default {
   name: 'AccountSetting',
   data() {
     return {
-      userInfo: {}
+      userInfo: {},
+      token: `Bearer ${JSON.parse(window.localStorage.getItem('user_info')).token}`
     }
   },
   created() {
@@ -75,9 +95,34 @@ export default {
         console.log(err)
         this.$message.error('保存用户信息失败')
       })
-    }
+    },
+    handleAvatarSuccess() {},
+    beforeAvatarUpload() {}
   }
 }
 </script>
 <style lang="less" scope>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>
